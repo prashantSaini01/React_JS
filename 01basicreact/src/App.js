9347be4521a5a48544943432113147148ab44310
prashantSaini01 <01prashantsaini01@gmail.com>
2024-09-02 18:19:15 +0530

    React Learning Phase 1

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello Today I Start learning React JS</h1>
+  )
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+
+  return (
+  <h1>Chai aur react with vite</h1>
+      
+  )
+}
+
+export default App
