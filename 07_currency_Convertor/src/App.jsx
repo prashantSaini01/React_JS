0c366feb881a8e84e7567a1539fbe7bf70980e81
prashantSaini01 <01prashantsaini01@gmail.com>
2024-09-04 18:20:04 +0530

    Currency Converter Project Added

---
+++ 07_currency_Convertor/src/App.jsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState('usd');
+  const [to, setTo] = useState('inr');
+  const [convertedAmount, setConvertedAmount] = useState(0);
+
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    const tempFrom = from;
+    const tempTo = to;
+    const tempAmount = amount;
+    const tempConvertedAmount = convertedAmount;
+
+    setFrom(tempTo);
+    setTo(tempFrom);
+    setAmount(tempConvertedAmount);
+    setConvertedAmount(tempAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
+  return (
+    <>
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('https://images.pexels.com/photos/259132/pexels-photo-259132.jpeg')`,
+        }}
+      >
+        <h1
+          className="text-7xl font-bold text-center mb-25 dollar-style"
+          style={{
+            fontFamily: "'Georgia', serif", // Mimicking the serif font used on dollar bills
+            color: '#1F2420', // Dark green color similar to dollar text
+            textShadow: '2px 2px 0 #ccc, 4px 4px 0 #aaa', // Subtle shadow to give it depth
+            letterSpacing: '2px', // Spaced out letters
+          }}
+        >
+          Currency Converter
+        </h1>
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  selectCurrency={from}
+                  onAmountChange={(amount) => setAmount(amount)}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  Swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                  label="To"
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectCurrency={to}
+                  amountDisable
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
